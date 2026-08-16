f833f2aede1e68fb7d5b67516f4ba124df156859
TitonouAnaniHounas <hounastitonou@gmail.com>
2026-08-16 22:05:01 +0000

    Initialisation

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import './style.css'
+import './custom.css'
+import { createIcons, Menu, X, Moon, Sun, Search, MapPin } from 'lucide'
+
+createIcons({
+  icons: { Menu, X, Moon, Sun, Search, MapPin },
+})
