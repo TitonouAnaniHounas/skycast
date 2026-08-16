6e492aa36889161d5b7e6d9e6ad51cfa8d73b3b3
TitonouAnaniHounas <hounastitonou@gmail.com>
2026-08-16 23:10:22 +0000

    page accueil

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import './style.css'
 import './custom.css'
-import { createIcons, Menu, X, Moon, Sun, Search, MapPin } from 'lucide'
+import { createIcons, Menu, X, Moon, Sun, Search, MapPin, Star, Droplet, Wind, Compass, Gauge, Eye, Sunrise, Sunset } from 'lucide'
 
 createIcons({
-  icons: { Menu, X, Moon, Sun, Search, MapPin },
+  icons: { Menu, X, Moon, Sun, Search, MapPin, Star, Droplet, Wind, Compass, Gauge, Eye, Sunrise, Sunset },
 })
