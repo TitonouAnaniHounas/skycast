97a168623e2350d0d1f5c08998b219a12773c75f
TitonouAnaniHounas <hounastitonou@gmail.com>
2026-08-19 01:15:52 +0000

    configuration pour deployage

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  base: '/skycast/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        favorites: resolve(__dirname, 'favorites.html'),
+        history: resolve(__dirname, 'history.html'),
+        settings: resolve(__dirname, 'settings.html'),
+        notFound: resolve(__dirname, '404.html'),
+      },
+    },
+  },
 })
